d066f2dc07446798df9f85424db5f0ebc6eda52f
William Queiroz <wnqueiroz@outlook.com>
2020-12-08 14:37:18 -0300

    fix: adjusting error when set TTL on Redis record

--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Inject, CACHE_MANAGER } from '@nestjs/common';
-import { Cache } from 'cache-manager';
+import { Cache, CachingConfig } from 'cache-manager';
 
 @Injectable()
 export class RedisService {
@@ -9,8 +9,15 @@ export class RedisService {
     return this.cache.get(key);
   }
 
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  async set(key: string, value: any, ttl: number): Promise<any> {
-    return this.cache.set(key, value, ttl);
+  async set(
+    key: string,
+    value: Record<string, unknown>,
+    ttl?: number,
+  ): Promise<any> {
+    const options: CachingConfig = {
+      ttl,
+    };
+
+    return this.cache.set(key, value, ttl && options);
   }
 }
